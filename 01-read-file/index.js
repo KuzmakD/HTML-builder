43e32c463fcf87256bb20520a085186586653cd6
Dmitry Korzun <korzundm7@gmail.com>
2022-05-22 21:12:47 +0300

    refactor: change code in 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = require('process');
+const { Stream } = require('stream');
 
-fs.readFile(
-   path.join(__dirname, 'text.txt'),
-   'utf-8',
-   (err, data) => {
-      if (err) throw err;
-      console.log(data);
-   }
-);
+const readableStream = fs.createReadStream(
+   path.join(__dirname, 'text.txt'), 'utf-8');
+
+readableStream.on('data', data => stdout.write(data));
+
+// or  readableStream.on('data', chunk => console.log(chunk));
+
+// fs.readFile(
+//    path.join(__dirname, 'text.txt'),
+//    'utf-8',
+//    (err, data) => { 
+//       if (err) throw err;
+//       console.log(data);
+//    }
+// );
